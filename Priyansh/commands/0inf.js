7e53fbc674e4dc2663c00a134724ed1ed61d4b4f
Rajput-v3 <shagunrajput723@gmail.com>
2025-03-11 22:38:39 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -24,16 +24,16 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://i.imgur.com/2n53Q8K.jpeg","https://i.imgur.com/AEBVH5D.jpeg"];
+["https://i.imgur.com/BRMZfP4.mp4","https://i.imgur.com/laOaa8f.mp4","https://i.imgur.com/mXH1gRe.mp4","https://i.imgur.com/2n53Q8K.jpeg","https://i.imgur.com/AEBVH5D.jpeg"];
 var callback = () => api.sendMessage({body:`🤍 𝘼𝘿𝙈𝙄𝙉 𝘼𝙉𝘿 𝘽❍𝙏 𝙄𝙉𝙁❍ 🤍
-𝙅𝘼𝙔 𝙎𝙃𝙍𝙀𝙀 𝙍𝘼𝙈➤𝙅𝘼𝙔 𝙈𝘼𝙃𝘼𝙆𝘼𝙇
+𝙅𝘼𝙔 𝙎𝙃𝙍𝙀𝙀 𝙍𝘼𝙈 ➤
 
-☄️𝘽𝙊𝙏 𝙉𝘼𝙈𝙀☄️ ➤  ${global.config.BOTNAME}
-✧══════•❁❀❁•══════✧
+☄️𝘽𝙊𝙏 𝙉𝘼𝙈𝙀☄️➤ ${global.config.BOTNAME}
+✧══════•❁❀❁•══════
 
 Bᴏᴛ Pʀᴇғɪx➤ ${global.config.PREFIX}
 
-❍ᴡɴᴇʀ➤𝙑𝙄𝙆𝘼𝙎▶𝙍𝙐𝘿𝙍𝘼 𝙍𝘼𝙅𝙋𝙐𝙏
+❍ᴡɴᴇʀ➤𝙑𝙄𝙆𝘼𝙎 ▶ 𝙍𝘼𝙅𝙋𝙐𝙏
 
 Uᴘᴛɪᴍᴇ 🤍
 
@@ -45,7 +45,7 @@ Tʜᴀɴᴋs Fᴏʀ Usɪɴɢ Mʏ ${global.config.BOTNAME} Bᴏᴛ🖤
 
 
 🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-   ▶𝙑𝙄𝙆𝘼𝙎➤𝙍𝙐𝘿𝙍𝘼 𝙍𝘼𝙅𝙋𝙐𝙏◀
+   ➤𝙍𝙐𝘿𝙍𝘼 𝙍𝘼𝙅𝙋𝙐𝙏◀
 
 
 `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
